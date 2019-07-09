a707022fc17168a6c6a480f582f342cfcbfab1b6
zhousg <zhoushugang@itcast.cn>
2019-07-09 12:34:06 +0800

    登录模块-绘制表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,13 +4,34 @@
     <el-card class="login_box">
       <img src="../../assets/images/logo_index.png" alt="">
       <!-- 表单 -->
-
+      <el-form>
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入验证码" style="width:280px"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked" ></el-checkbox>
+          我已阅读并同意 <el-link type="primary">用户协议</el-link> 和 <el-link type="primary">隐私条款</el-link>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary">登 录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      checked: true
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
@@ -31,8 +52,11 @@ export default {}
     img{
       display: block;
       width: 200px;
-      margin: 0 auto;
+      margin: 10px auto 30px ;
     }
   }
+}
+.el-checkbox{
+  margin-right: 5px;
 }
 </style>
