64e65c2e575c83350159c931075f5e09d154e499
zhousg <zhoushugang@itcast.cn>
2019-07-09 12:20:46 +0800

    登录模块-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,38 @@
 <template>
-  <div class='container'>Login</div>
+  <div class='login-container'>
+    <!-- 卡片 -->
+    <el-card class="login_box">
+      <img src="../../assets/images/logo_index.png" alt="">
+      <!-- 表单 -->
+
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.login-container{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .login_box{
+    width: 450px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    img{
+      display: block;
+      width: 200px;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
